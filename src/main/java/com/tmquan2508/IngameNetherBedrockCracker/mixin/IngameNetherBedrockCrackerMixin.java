f5af0d6172b7f396207be8380ba83137ef0afa2b
Kudo <tmquan2508@gmail.com>
2025-05-24 12:59:01 +0200

    :D

--- src/main/java/com/tmquan2508/IngameNetherBedrockCracker/mixin/IngameNetherBedrockCrackerMixin.java
+++ src/main/java/com/tmquan2508/IngameNetherBedrockCracker/mixin/IngameNetherBedrockCrackerMixin.java
@@ -2,9 +2,6 @@ package com.tmquan2508.IngameNetherBedrockCracker.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
-// import org.spongepowered.asm.mixin.injection.At;
-// import org.spongepowered.asm.mixin.injection.Inject;
-// import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class IngameNetherBedrockCrackerMixin {
